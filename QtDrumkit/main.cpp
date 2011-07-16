71daa4480afb82782d577ea58cc49efca96c4335
K-P Koljonen <kkol@futurice.com>
2011-07-16 14:41:38 +0300

    .

--- QtDrumkit/main.cpp
+++ QtDrumkit/main.cpp
@@ -1,6 +1,6 @@
 #include <QDebug>
 #include <QtGui/QApplication>
-//#include <QGLWidget>
+#include <QGLWidget>
 #include <QtDeclarative>
 
 #include "qmlapplicationviewer.h"
@@ -40,12 +40,12 @@ int main(int argc, char *argv[])
 
     // For N900 and Harmattan set OpenGL rendering
 #if defined(Q_WS_MAEMO_5) || defined(Q_WS_MAEMO_6)
-//    QGLFormat fmt = QGLFormat::defaultFormat();
-//    fmt.setDirectRendering(true);
-//    fmt.setDoubleBuffer(true);
+    QGLFormat fmt = QGLFormat::defaultFormat();
+    fmt.setDirectRendering(true);
+    fmt.setDoubleBuffer(true);
 
-//    QGLWidget *glWidget = new QGLWidget(fmt);
-//    viewer.setViewport(glWidget);
+    QGLWidget *glWidget = new QGLWidget(fmt);
+    viewer.setViewport(glWidget);
 #endif
 
 #if defined(Q_WS_MAEMO_6) || defined(Q_OS_SYMBIAN)
